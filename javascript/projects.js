718301314d65a4e5210a0e5e89608a0ef51c0a65
egom1401 <egom1401@usherbrooke.ca>
2025-06-13 15:34:52 -0400

    Modified frontend mostly

--- javascript/projects.js
+++ javascript/projects.js
@@ -2,10 +2,10 @@
 const months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
 
 // Fonctions
-async function getUserGithubRepos(username){
+async function getUserGithubRepos(user){
   try
   {
-    const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&direction=desc`);
+    const response = await fetch(`https://api.github.com/users/${user}/repos?sort=updated&direction=desc`);
     const userRepos = await response.json();
     return userRepos;
   }
@@ -16,31 +16,40 @@ async function getUserGithubRepos(username){
   }
 }
 
-async function createGitHubHTML(username){
-  const userRepos = await getUserGithubRepos(`${username}`);
-  const projetdiv = document.getElementById('mainContent')
+// TODO: Ajouter les 5 langages de programmation les plus utilisés.
+async function getProgrammingLanguageGitHubRepo(user, userRepo){
+  try
+  {
+    const response = await fetch(`https://api.github.com/repos/${user}/${userRepo}/languages`);
+    const repoProgrammingLanguage = await response.json();
+    return repoProgrammingLanguage;
+  }
+
+  catch(error)
+  {
+    throw new Error("Couldn't get the programming languages form the repo.");
+  }
+}
+
+async function createGitHubHTML(user){
+  const projetdiv = document.getElementById('main')
+  const userRepos = await getUserGithubRepos(user);
 
   userRepos.forEach(element => {
     let date = new Date(element.pushed_at);
-    let day = date.getDate().toString();
-    let month =  date.getMonth().toString();
-    let year = date.getFullYear().toString();
-
-    let description = element.description === null ? "(Ce répositoire ne contient pas de description)": element.description;
-
-    console.log(description);
+    let description = element.description === null ?
+                      "À implémenter dans GitHub" : element.description;
 
     projetdiv.innerHTML += `<div class="mainContentSection">
                               <div class="mainContentSectionHeader">
                                 <a href=${element.html_url} target="_blank">${element.name}</a>
-                                <i>Derniers changements: ${day} ${months[month]} ${year}</i>
+                                <i>Derniers changements: ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}</i>
                               </div>
                               <hr>
                               <p>${description}</p>
                             </div>`;
-  });
 
-  console.log(userRepos);
+  });
 }
 
 createGitHubHTML('MaximeEgolf');
