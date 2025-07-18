1643a1d99d1d2d73fa8a044c4107838c708d237a
egom1401 <egom1401@usherbrooke.ca>
2025-07-18 15:43:39 -0400

    Modified Node.js with Express.js (error, input, static files etc..)

--- public/cmd.js
+++ public/cmd.js
@@ -4,27 +4,31 @@ cmd.addEventListener('blur', () => {
   cmd.focus();
 });
 
-cmd.addEventListener('keydown', async (event) => {
-  if (event.key === "Enter")
-  {
-    if (cmd.value.length && cmd.value.startsWith("cd"))
-    {
-      try {
-        const cdRes = await fetch('http://localhost:3000/api/cd', {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify(cmd.value)
-        });
+// Et si j'utilisais ça à la place de mettre un <form> en html ?
+// Ça permettrait de faire une première vérification (moins de call à l'API)
 
-        const cdResJson  = await cdRes.json();
-        if (cdResJson.from != '/api/cd')
-          throw new Error(`Called api ${cdResJson.from} instead of /api/cd`);
-      }
-      catch (error)
-      {
-        console.error(error);
-      }
-    }
-    cmd.value = '';
-  }
-});
+// cmd.addEventListener('keydown', async (event) => {
+//   if (event.key === "Enter")
+//   {
+//     // if (cmd.value.length && cmd.value.startsWith("cd"))
+//     // {
+//       try {
+//         console.log(cmd.value);
+//         const cdRes = await fetch('http://localhost:3000/api/cd', {
+//           method: 'POST',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({ value: cmd.value })
+//         });
+
+//         // const cdResJson  = await cdRes.json();
+//         // if (cdResJson.from != '/api/cd')
+//         //   throw new Error(`Called api ${cdResJson.from} instead of /api/cd`);
+//       }
+//       catch (error)
+//       {
+//         console.error(error);
+//       }
+//     // }
+//     cmd.value = '';
+//   }
+// });
--- server.js
+++ server.js
@@ -1,115 +1,40 @@
-import http from 'http';
 import path from 'path';
-import fs from 'fs';
 import url from 'url';
 import {github} from './api.js';
+
+import express from 'express';
+const app = express();
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Constants
-const PORT = process.env.PORT;
-const MIME_TYPES = {
-  '.html': 'text/html',
-  '.css': 'text/css',
-  '.js': 'text/javascript',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.jpeg': 'image/jpeg',
-  '.pdf': 'application/pdf'
-};
+const PORT = process.env.PORT || 3000;
 
-const ERROR_MESSAGE = {
-  400: 'Mauvaise requête - La demande est invalide',
-  401: 'Non autorisé - Authentification requise',
-  403: 'Interdit - Accès refusé',
-  404: 'Page non trouvée - La ressource demandée n\'existe pas',
-  405: 'Méthode non autorisée - Cette méthode HTTP n\'est pas supportée',
-  408: 'Délai d\'attente dépassé - La requête a pris trop de temps',
-  429: 'Trop de requêtes - Veuillez réessayer plus tard',
-  500: 'Erreur interne du serveur - Un problème est survenu',
-  502: 'Mauvaise passerelle - Erreur de serveur distant',
-  503: 'Service indisponible - Le service est temporairement indisponible',
-  504: 'Délai d\'attente de la passerelle - Le serveur distant ne répond pas'
-};
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-function getContentType(filePath){
-  let ext = path.extname(filePath);
-  return MIME_TYPES[ext] || 'text/plain';
-}
-
-function getMessageFromStatus(statusCode){
-  return ERROR_MESSAGE[statusCode] || 'Erreur non connue';
-}
-
-// Error handling
-function errorPage(res, statusCode){
-  const errorFilePath = path.join(__dirname, 'public', 'error.html');
-  fs.readFile(errorFilePath, 'utf8', (error, data) => {
-    if (error)
-    {
-      res.writeHead(statusCode, {'Content-Type': 'text/html'});
-      res.end();
-    }
-    else
-    {
-      const newData = data.replace('${status-code}', statusCode)
-                          .replace('${error-message}', getMessageFromStatus(statusCode));
-      const contentType = getContentType(errorFilePath);
-      res.writeHead(200, {'Content-Type': contentType});
-      res.end(newData);
-    }
-  })
-}
-
-// Server
-const server = http.createServer(async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  let requestPath = req.url;
-
-  if (req.method === 'GET') {
-    if (requestPath === '/') {
-      requestPath = 'homepage.html';
-    }
-    else if (requestPath === '/api/github') {
-      const githubRes = await github(process.env.USER, process.env.GITHUB_TOKEN);
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({from: requestPath, value: githubRes}));
-      return;
-    }
-
-    const filePath = path.join(__dirname, 'public', requestPath);
-    fs.readFile(filePath, (error, data) => {
-      if (error) {
-        errorPage(res, 404);
-      }
-      else {
-        const contentType = getContentType(filePath);
-        res.writeHead(200, {'Content-Type': contentType});
-        res.end(data);
-      }
-    })
-  }
-  else if (req.method === "POST")
-  {
-    if (requestPath === "/api/cd")
-    {
-      // TO DO
-      // get the body message!
-
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({from: requestPath, value: "TODO" }));
-      return;
-    }
-  }
-  else {
-    errorPage(res, 405);
-  }
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'homepage.html'));
 });
 
-server.listen(PORT, (error) => {
+app.get("/api/github", async (req, res) =>
+{
+  const githubRes = await github(process.env.USER, process.env.GITHUB_TOKEN);
+  res.json({from: req.path, value: githubRes});
+});
+
+app.post("/api/cd", (req, res) =>
+{
+  res.redirect(`/${req.body.value}.html`);
+});
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'public', 'error.html'));
+});
+
+app.listen(PORT, (error) => {
   if (error)
     console.log(`Server isn't running because of error: ${error}`);
   else
